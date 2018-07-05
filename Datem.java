39f0b97b80371c6731dd42b5b11db588a87de8d1
chinmayek <40786106+chinmayek@users.noreply.github.com>
2018-07-05 19:49:55 +0530

    Add files via upload

---
+++ Datem.java
@@ -0,0 +1,9 @@
+package assignment5;
+
+public class Datem {
+    public static void main(String args[]){
+        Dat d1=new Dat();
+        Dat d2=new Dat(5,7,2018);
+        d2.alter();
+    }
+}
