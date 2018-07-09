92a4626e0db5af5eddfdeb89051411942a41fe8c
chinmayek <40786106+chinmayek@users.noreply.github.com>
2018-07-09 13:17:42 +0530

    Add files via upload

---
+++ pro5/Datemain.java
@@ -0,0 +1,9 @@
+package assignment5;
+
+public class Datemain {
+    public static void main(String args[]){
+        Date d1=new Date();
+        Date d2=new Date(5,7,2018);
+        d2.alter();
+    }
+}
