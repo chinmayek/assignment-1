6e08f1dbc9fce04e970909af1918b6cc7ad30068
chinmayek <40786106+chinmayek@users.noreply.github.com>
2018-07-09 18:03:33 +0530

    Delete Datem.java

--- Datem.java
+++
@@ -1,9 +0,0 @@
-package assignment5;
-
-public class Datem {
-    public static void main(String args[]){
-        Dat d1=new Dat();
-        Dat d2=new Dat(5,7,2018);
-        d2.alter();
-    }
-}
