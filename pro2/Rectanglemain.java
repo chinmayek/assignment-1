5148f06b6030caa72cdaa995be100ec74578e505
chinmayek <40786106+chinmayek@users.noreply.github.com>
2018-07-09 13:14:53 +0530

    Add files via upload

---
+++ pro2/Rectanglemain.java
@@ -0,0 +1,32 @@
+package pro2;
+
+import pro2.Rectangle;
+
+import java.util.Scanner;
+
+public class Rectanglemain {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        Rectangle obj[]=new Rectangle[5];
+        for(int i=0; i<5; i++){
+            System.out.println("enter info about obj"+(i+1));
+            System.out.println("enter the length");
+            float len=sc.nextFloat();
+            System.out.println("enter the bredth");
+            float bre=sc.nextFloat();
+
+            obj[i]=new Rectangle(len,bre);
+            obj[i].setLen(len);
+            obj[i].setBre(bre);
+        }
+
+        for(int i=0;i<5;i++){
+            System.out.println("Info about obj"+(i+1));
+            System.out.println("length is "+obj[i].getLen());
+            System.out.println("breadth is "+obj[i].getBre());
+            System.out.println("area is "+obj[i].area());
+        }
+
+    }
+}
